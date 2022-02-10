7db8427330ec91aad0a3d4bf920ecef3ea675267
winnie59 <kwang_kwah_noi@yahoo.com>
2022-02-09 20:11:50 -0500

    user login and signup front end and backend are completed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Login.css'
 import { useState } from 'react';
+import Google from '../Google';
+import Signup from '../Signup/Signup';
 
 
-const Login = () => {
-// sign up
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [verifyPassword, setVerifyPassword] = useState('')
+const Login = ({setUser}) => {
+  const [users, setUsers] = useState([])
+  
+  let getUsers = async () => {
+	  let data = await fetch('http://localhost:4004/users')
+	  let allUsers = await data.json()
+	  setUsers(allUsers)
+  }
 
-//  login
+  useEffect(() => {
+	  getUsers()
+  },[])
+
+  let addUsers = (user) => {
+	  setUsers([...users,user])
+  }
+
+
   const initialState = {usernameToLogIn: '',passwordToLogIn:''}
   const [formState, setFormState] =useState(initialState)
   const handleChange = e =>{
@@ -20,24 +32,24 @@ const Login = () => {
 	  })
   }
 
-//   login
-  const handleSubmitLogIn = e =>{
+  const handleSubmitLogIn = async (e) =>{
 	e.preventDefault()
 	alert(`Welcome back ${formState.usernameToLogIn}! Enjoy the Recipies!`)
+	let data = await fetch(`http://localhost:4004/users/login`,{
+		method: 'POST',
+		body: JSON.stringify({
+			username: formState.usernameToLogIn,
+			password: formState.passwordToLogIn
+		}),
+		headers: {
+			'content-type': 'application/json'
+		}
+	})
+	let userData = await data.json()
+	setUser(userData)
 	setFormState(initialState)
 }
 
-//  sign up
-  const handleSubmitSignUp = e =>{
-      e.preventDefault()
-    //   if (verifyPassword.current.value !== password.current.value) {
-    //     verifyPassword.current.setCustomValidity("Passwords don't match!")
-	// 	alert("Passwords don't match!")
-    //   }else{
-		alert(`${username} Welcome to Social-Meal-Dia! Enjoy the Recipies`)
-	//   }
-
-  }
 
   return (
     <div className='login-body'>
@@ -45,25 +57,13 @@ const Login = () => {
 <div className="main">  	
 		<input type="checkbox" id="chk" aria-hidden="true"/>
 
-
-			<div className="signup">
-				<form onSubmit={handleSubmitSignUp}>
-					<label htmlFor="chk" aria-hidden="true">Sign up</label>
-
-					<input type="text" id="username" name="txt" placeholder="User name" required="" value={username} onChange={e => setUsername(e.target.value)}/>
-					<input type="email" id="email" name="email" placeholder="Email" required="" value={email} onChange={e=> setEmail(e.target.value)} />
-
-					<input type="password" id="password" name="pswd" placeholder="Password" required="" value={password} onChange={e => setPassword(e.target.value)}/>
-
-          <input type="password" id="verifyPassword" name="pswd" placeholder="Verify Password" required="" value={verifyPassword} onChange={e => setVerifyPassword(e.target.value)} />
-					<button className='login-button'>Sign up</button>
-				</form>
-			</div>
+		<Signup addUsers={addUsers}/>
 
 			<div className="login">
 				<form onSubmit={handleSubmitLogIn}>
 					<label htmlFor="chk" aria-hidden="true">Login</label>
-
+					<Google />
+					<h2>OR</h2>
 					<input type="username" id="usernameToLogIn" name="username" placeholder="User Name" required="" value={formState.usernameToLogIn} onChange={handleChange}/>
 
 					<input type="password" id="passwordToLogIn" name="pswd" placeholder="Password" required="" value={formState.passwordToLogIn} onChange={handleChange}/>
