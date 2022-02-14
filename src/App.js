b161a9a642000ded9993a607ae89b4e0062c42ee
nicholastonelli <nicholasevantonelli@gmail.com>
2022-02-13 21:12:30 -0500

    axios on userpage implemented

--- src/App.js
+++ src/App.js
@@ -1,60 +1,59 @@
 // import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header';
-import {Route, Routes, Link} from 'react-router-dom'
-import Nav from './Components/Nav/Nav';
-import New from './Components/New/New';
+import "./App.css"
+import Header from "./Components/Header"
+import { Route, Routes, Link } from "react-router-dom"
+import Nav from "./Components/Nav/Nav"
+import New from "./Components/New/New"
 
-import axios from 'axios'
+import axios from "axios"
 
 // import { Route,Routes,Link } from 'react-router-dom';
-import Posts from './Components/Posts';
-import SaveDishes from './Components/SaveDishes/SaveDishes';
-import Feed from './Components/Feed/Feed';
-import Chat from './Components/Chat/Chat';
-import Home from './Components/Home/Home';
-import Login from './Components/Login/Login';
-import Recipe from './Components/Recipe/Recipe';
-import { useEffect, useState } from 'react';
-import User from './Components/User'
-import NewForm from './Components/NewForm/NewForm';
-import ProtectedRoutes from './Components/ProtectedRoutes';
-import { getDefaultNormalizer } from '@testing-library/dom'
+import Posts from "./Components/Posts"
+import SaveDishes from "./Components/SaveDishes/SaveDishes"
+import Feed from "./Components/Feed/Feed"
+import Chat from "./Components/Chat/Chat"
+import Home from "./Components/Home/Home"
+import Login from "./Components/Login/Login"
+import Recipe from "./Components/Recipe/Recipe"
+import { useEffect, useState } from "react"
+import User from "./Components/User"
+import NewForm from "./Components/NewForm/NewForm"
+import ProtectedRoutes from "./Components/ProtectedRoutes"
+import { getDefaultNormalizer } from "@testing-library/dom"
 
-console.log(process.env.REACT_APP_backendURI)
-
-
-function App({postData, userData}) {
-
+function App({ postData, userData }) {
   const [posts, setPosts] = useState([])
   const [user, setUser] = useState()
-  const [message,setMessage] = useState('')
+  const [message, setMessage] = useState("")
   // const [isLogin, setIsLogin] = useState(false)
+  const [users, setUsers] = useState()
+  const [foundUser, setFoundUser] = useState()
 
-
-   function getData() {
-    axios.get(`${process.env.REACT_APP_backendURI}posts`)
-    .then(res => {
+  function getData() {
+    axios.get(`${process.env.REACT_APP_backendURI}posts`).then((res) => {
       setPosts(res.data)
     })
-  } 
+    axios.get(`${process.env.REACT_APP_backendURI}users`).then((res) => {
+      setUsers(res.data)
+    })
+  }
 
   useEffect(() => {
     getData()
-    },[]) 
- 
+  }, [])
+
   const login = (user) => {
     if (user && user.username !== undefined) {
-      console.log('login')
+      console.log("login")
       setUser(user)
       // setIsLogin(!isLogin)
       // console.log(!isLogin)
       console.log(user)
       setMessage(`Log in Success`)
-    }else {
+    } else {
       setMessage(`Wrong username or password`)
     }
-  } 
+  }
 
   //  const logout = () => {
   //   setUser({username:'', password:''})
@@ -70,7 +69,7 @@ function App({postData, userData}) {
   let tempUser = userData[0]
   return (
     <div className="App">
- {/* <button onClick={logout}>logout</button>  */}
+      {/* <button onClick={logout}>logout</button>  */}
       <Nav user={user} />
         <Routes>
           <Route path='/' element={<Home posts={posts} user={user}/>}/>
@@ -88,7 +87,7 @@ function App({postData, userData}) {
       {/* <Posts postData={posts}/> */}
       {/* <Recipe/> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,18 +1,36 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import axios from "axios"
 
-const User = ({ user }) => {
+const User = ({ setFoundUser, foundUser, users }) => {
+  const params = useParams()
+  console.log(params.userId)
+  console.log(`${process.env.REACT_APP_backendURI}users/${params.userId}`)
   
-    
+  function getUser() {
+    axios.get(`${process.env.REACT_APP_backendURI}users/${params.userId}`)
+    .then((res) => {
+      console.log(res)
+      setFoundUser(res.data)
+    })
+  }
+
+  useEffect((getUser) => {
+    getUser()
+  }, [])
+
+ console.log(foundUser)
+
   return (
     <div>
       <header>
         <img src="" alt="" />
-        <h4>{user.name}</h4>
+        <h4>{foundUser.username}</h4>
         {/* Below code only lists the number of posts, mapping the data is found lower */}
-        <p>{user.posts.length} posts</p>
-        <p>{user.followers}  followers</p>
-        <p>{user.following} following</p>
+        <p>{foundUser.posts} posts</p>
+        <p>{foundUser.followers} followers</p>
+        <p>{foundUser.following} following</p>
         <hr></hr>
       </header>
 
