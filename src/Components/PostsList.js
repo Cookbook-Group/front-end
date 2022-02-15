478fdb09242d3808e63b48697410cd2ad07d7bfa
winnie59 <kwang_kwah_noi@yahoo.com>
2022-02-14 21:07:26 -0500

    delete post is now working and update sign in auth and update some page layout

--- src/Components/PostsList.js
+++ src/Components/PostsList.js
@@ -3,12 +3,20 @@ import { useState,useEffect } from 'react/cjs/react.development';
 import Post from './post/Post';
 import axios from 'axios';
 
+<<<<<<< HEAD
 const PostsList = ({postData,user,setPosts, getData}) => {
+=======
+const PostsList = ({postData,user,setPosts}) => {
+>>>>>>> 0c97d53 (delete post is now working and update sign in auth and update some page layout)
 
   return <div>
       {postData.map((post) =>{
         
+<<<<<<< HEAD
          return <Post post={post} user={user} setPosts={setPosts} key={post.name} getData={getData}/>
+=======
+         return <Post post={post} user={user} setPosts={setPosts} key={post.name}/>
+>>>>>>> 0c97d53 (delete post is now working and update sign in auth and update some page layout)
       })}
       
   </div>;
--- src/Components/post/Post.js
+++ src/Components/post/Post.js
@@ -9,7 +9,11 @@ import axios from 'axios';
 
 
 
+<<<<<<< HEAD
 const Post = ({post,user,likeHandler,like, setPosts , getData}) => {
+=======
+const Post = ({post,user,likeHandler,like, setPosts}) => {
+>>>>>>> 0c97d53 (delete post is now working and update sign in auth and update some page layout)
 
   const [isOpen, setIsOpen] = useState(false);
   const [currentUser, setCurrentUser] = useState()
@@ -43,7 +47,10 @@ const Post = ({post,user,likeHandler,like, setPosts , getData}) => {
     alert('Your post had been deleted.')
     if (allPosts) {
     setPosts(allPosts)
+<<<<<<< HEAD
     // getData()
+=======
+>>>>>>> 0c97d53 (delete post is now working and update sign in auth and update some page layout)
     }
     } else {
       alert('You can only delete your own post.')
